a36c7def8c79daa24f2064219829b6aa3e0adb13
nma044 <nathanielm@lyse.net>
2022-08-31 15:12:54 +0200

    Added openApi documentation for methods

--- src/main/java/no/noroff/assignment3/controllers/CharacterController.java
+++ src/main/java/no/noroff/assignment3/controllers/CharacterController.java
@@ -1,7 +1,13 @@
 package no.noroff.assignment3.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import no.noroff.assignment3.mappers.CharacterMapper;
 import no.noroff.assignment3.moduls.Character;
+import no.noroff.assignment3.moduls.Franchise;
 import no.noroff.assignment3.moduls.dtos.CharacterDTO;
 import no.noroff.assignment3.services.Character.CharacterService;
 import org.springframework.http.HttpStatus;
@@ -21,11 +27,31 @@ public class CharacterController {
         this.characterMapper = characterMapper;
     }
 
+    @Operation(summary = "Get all characters")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Character.class)) }),
+            @ApiResponse(responseCode = "404",
+                    description = "characters not found",
+                    content = @Content)
+    })
     @GetMapping
     public ResponseEntity<Collection<Character>> getAll() {
         return ResponseEntity.ok(characterService.findAll());
     }
 
+    @Operation(summary = "Get character by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Character.class)) }),
+            @ApiResponse(responseCode = "404",
+                    description = "character not found by supplied id",
+                    content = @Content)
+    })
     @GetMapping("{id}")
     public ResponseEntity getById(@PathVariable int id) {
         Character character = characterService.findById(id);
@@ -33,18 +59,48 @@ public class CharacterController {
         return ResponseEntity.ok(characterDTO);
     }
 
+    @Operation(summary = "Add character")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Character.class)) }),
+            @ApiResponse(responseCode = "404",
+                    description = "character not found",
+                    content = @Content)
+    })
     @PostMapping
     public ResponseEntity add(@RequestBody Character character) {
         characterService.add(character);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "Delete character by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Character.class)) }),
+            @ApiResponse(responseCode = "404",
+                    description = "character not found by supplied id",
+                    content = @Content)
+    })
     @DeleteMapping("{id}")
     public ResponseEntity delete(@PathVariable int id) {
         characterService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "update character by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Character.class)) }),
+            @ApiResponse(responseCode = "404",
+                    description = "character not found by supplied id",
+                    content = @Content)
+    })
     @PutMapping("{id}")
     public ResponseEntity update(@RequestBody Character character, @PathVariable int id) {
         // Validates if body is correct
--- src/main/java/no/noroff/assignment3/controllers/MovieController.java
+++ src/main/java/no/noroff/assignment3/controllers/MovieController.java
@@ -1,5 +1,10 @@
 package no.noroff.assignment3.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import no.noroff.assignment3.mappers.MovieMapper;
 import no.noroff.assignment3.moduls.Movie;
 import no.noroff.assignment3.moduls.dtos.MovieDTO;
@@ -29,11 +34,31 @@ public class MovieController {
         this.movieMapper = movieMapper;
     }
 
+    @Operation(summary = "Get all movies")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Movie.class)) }),
+            @ApiResponse(responseCode = "404",
+                    description = "Could not find movies",
+                    content = @Content)
+    })
     @GetMapping
     public ResponseEntity<Collection<Movie>> getAll() {
         return ResponseEntity.ok(movieService.findAll());
     }
 
+    @Operation(summary = "Get a Movie by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Movie.class)) }),
+            @ApiResponse(responseCode = "404",
+                    description = "Movie does not exist with supplied ID",
+                    content = @Content)
+    })
     @GetMapping("{id}")
     public ResponseEntity getById(@PathVariable int id) {
         Movie movie = movieService.findById(id);
@@ -42,18 +67,48 @@ public class MovieController {
     }
 
 
+    @Operation(summary = "Add new movie")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Movie.class)) }),
+            @ApiResponse(responseCode = "404",
+                    description = "Could not find movie to add",
+                    content = @Content)
+    })
     @PostMapping
     public ResponseEntity add(@RequestBody Movie movie) {
         movieService.add(movie);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "delete a Movie by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Movie.class)) }),
+            @ApiResponse(responseCode = "404",
+                    description = "Movie does not exist with supplied ID",
+                    content = @Content)
+    })
     @DeleteMapping("{id}")
     public ResponseEntity delete(@PathVariable int id) {
         movieService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "update a Movie by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Movie.class)) }),
+            @ApiResponse(responseCode = "404",
+                    description = "Movie does not exist with supplied ID",
+                    content = @Content)
+    })
     @PutMapping("{id}")
     public ResponseEntity update(@RequestBody Movie movie, @PathVariable int id) {
         // Validates if body is correct
@@ -63,6 +118,16 @@ public class MovieController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "update characters in a Movie by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Movie.class)) }),
+            @ApiResponse(responseCode = "404",
+                    description = "Movie does not exist with supplied ID",
+                    content = @Content)
+    })
     @PutMapping("{id}/characters")
     public ResponseEntity updateCharacters(@RequestBody int[] characterId, @PathVariable int id) {
         // Validates if body is correct
@@ -76,6 +141,16 @@ public class MovieController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Get characters in a Movie by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Movie.class)) }),
+            @ApiResponse(responseCode = "404",
+                    description = "Movie does not exist with supplied ID",
+                    content = @Content)
+    })
     @GetMapping("{id}/characters")
     public ResponseEntity<Collection<Character>> getAllCharacters(@PathVariable int id) {
         return ResponseEntity.ok(movieService.findById(id).getCharacters());
